d57cdc68b69e706b9ab02d650abaff8aa28217b7
Anthony Branham <70983756+TonZaga@users.noreply.github.com>
2025-05-13 08:39:45 -0400

    Update BGG_Collection_Generator.py
    Cleaned up formatting and added clarity to comments

--- BGG_Collection_Generator.py
+++ BGG_Collection_Generator.py
@@ -4,19 +4,21 @@ import re
 import webbrowser
 from bs4 import BeautifulSoup
 
-pd.set_option('display.max_colwidth', None)
-
+# Get the BGG collection data for a given username
 username = input("Enter BGG username: ")
 base_url = f"https://boardgamegeek.com/collection/user/{username}?objecttype=thing&ff=1&subtype=boardgame"
 response = requests.get(base_url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
+
 # Find the table containing the collection data
 table = soup.find('table', class_='collection_table')
+
 # Find all rows in the table
 rows = table.find_all('tr')
+
 # Initialize an empty list to store the headers
 headers = []
+
 # Find the header row and extract the headers
 header_row = rows[0]
 header_cells = header_row.find_all('th')
@@ -37,16 +39,16 @@ for row in rows[1:]:  # Skip the header row
 # Pandas DataFrame to store the data
 df = pd.DataFrame(data, columns=headers)
 
-# Drop unnecessary columns
+# Drop unwanted/blank columns
 df = df.drop(columns=['Version', 'UserRating', 'UserPlays'])
 
-# Replace N/A strings with 0
+# Replace N/A strings with 0 in Geek rating column
 df = df.replace('N/A', 0)
 
-# Round numeric columns to 2 decimal places
+# Round Geek Rating to 2 decimal places
 df['Geek Rating'] = df['Geek Rating'].astype(float).round(2)
 
-# Remove dates from the 'Comment' column
+# Remove comment dates from the 'Comment' column
 df['Comment'] = df['Comment'].str[:-10]
 
 # Correct spacing issues in the 'Comment' column
@@ -54,15 +56,14 @@ def split_by_caps(text):
     return re.findall('[A-Z][^A-Z]*', text)
 
 df['Comment'] = df['Comment'].apply(split_by_caps).astype("string")
-
 df['Comment'] = df['Comment'].str.strip('[]')
 df['Comment'] = df['Comment'].str.replace("'", '')
 
+# Remove parentheses and text within from the 'Title' column
 def remove_parentheses(text):
     return re.sub(r'\(.*?\)', '', str(text))
 
 df['Title'] = df['Title'].apply(remove_parentheses)
-
 df['Title'] = df['Title'].str.rstrip()
 
 # Print the DataFrame to see the data
@@ -70,7 +71,7 @@ df['Title'] = df['Title'].str.rstrip()
 
 # Choice of HTML or export to CSV
 while True:
-    delivery = input("Select H for html or C for csv file\n")
+    delivery = input("Select 'H' for html or 'C' for csv file\n")
     upper_input = delivery.upper()
 
 # Write and display HTML file
